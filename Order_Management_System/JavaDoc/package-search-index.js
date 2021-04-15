f266e7855c0bedf9af8cd2b50b2fb204d9ac9de7
Șandru Diana <76928660+dianasandru08@users.noreply.github.com>
2021-04-15 18:19:30 +0300

    Delete Order_Management_System directory

--- Order_Management_System/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"BLC"},{"l":"bll"},{"l":"bll.validatori"},{"l":"connection"},{"l":"dao"},{"l":"model"},{"l":"presentation"},{"l":"start"}]
